9ef272c28568b4aaa9215df49357bb023c611f69
C. Reid <ctlnrd@gmail.com>
2017-05-09 18:30:10 -0400

    linking pages via get requests

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,11 @@
 // 3. Your `htmlRoutes.js` file should include two routes:
 
-//    * A GET Route to `/survey` which should display the survey page.
-//    * A default USE route that leads to `home.html` which displays the home page.
+// * A GET Route to `/survey` which should display the survey page.
+app.get("/survey", function(req, res) {
+  res.send("Welcome to the Survey!");
+});
+
+
+
+
+// * A default USE route that leads to `home.html` which displays the home page.
